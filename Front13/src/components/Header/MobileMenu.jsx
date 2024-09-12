afb99d15d649407fdc118ffa92191dd2072cbf39
Elenadelatorre <elenadelatorre1806@gmail.com>
2024-09-12 14:56:11 +0200

    Mejoras de diseño

--- Front13/src/components/Header/MobileMenu.jsx
+++ Front13/src/components/Header/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, IconButton, Flex, Button  } from '@chakra-ui/react';
+import { Box, IconButton, Flex, Button, useDisclosure } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import NavLink from './NavLink';
 import ChangeTheme from '../ChangeTheme/ChangeTheme';
@@ -21,70 +21,105 @@ const MobileMenu = ({
       ml={4}
       position='absolute'
       left={2}
+      zIndex='modal'
     />
+    <Box
+      as='img'
+      src='./assets/moto.png'
+      alt='Logo'
+      height='70px'
+      ml={200}
+      display={{ base: 'flex', md: 'none' }}
+      onClick={onOpen}
+    />
+    
+    {/* Overlay */}
+    {isOpen && (
+      <Box
+        position='fixed'
+        top='0'
+        left='0'
+        right='0'
+        bottom='0'
+        bg='rgba(0, 0, 0, 0.5)' // Fondo semi-transparente
+        zIndex='overlay'
+        onClick={onClose} // Cierra el menú cuando se hace clic en el overlay
+      />
+    )}
 
     <Flex
-      display={{ base: 'flex', md: 'none' }}
-      flexDirection='column'
-      alignItems='center'
+      direction='column'
+      align='center'
       p={4}
       bg='white'
       boxShadow='md'
       borderRadius='md'
+      position='fixed' // Cambiado de 'absolute' a 'fixed'
+      top='0'
+      left='0'
+      right='0'
+      zIndex='modal'
+      display={isOpen ? 'flex' : 'none'}
+      opacity={isOpen ? 1 : 0}
+      transition='opacity 0.3s ease'
+      height='100vh' // Asegura que el menú ocupe toda la altura de la pantalla
     >
-      <Box as='img' src='./assets/moto.png' alt='Logo' height='70px' mb={4} />
-      <NavLink href='/' onClick={onClose} inDrawer={true}>
-        Inicio
-      </NavLink>
-      <NavLink href='/motos' onClick={onClose} inDrawer={true}>
-        Alquiler de Motos
-      </NavLink>
-      {isAuthenticated && (
-        <>
-          <NavLink href='/myMotos' onClick={onClose} inDrawer={true}>
-            Mis reservas y reseñas
-          </NavLink>
-          <NavLink href='/AddMoto' onClick={onClose} inDrawer={true}>
-            Añadir moto
-          </NavLink>
-        </>
-      )}
-      <NavLink href='/suscripcion' onClick={onClose} inDrawer={true}>
-        Ofertas Exclusivas
-      </NavLink>
-      <Box mt={4}>
-        {isAuthenticated ? (
-          <Button
-            colorScheme='red'
-            onClick={() => {
-              logout();
-              onClose();
-            }}
-            size='sm'
-            width='full'
-          >
-            Cerrar Sesión
-          </Button>
-        ) : (
-          location.pathname !== '/login' && (
+      <Flex direction='column' align='center' flex='1'>
+        <Box as='img' src='./assets/moto.png' alt='Logo' height='70px' mb={4} />
+        <NavLink href='/' onClick={onClose} inDrawer={true}>
+          Inicio
+        </NavLink>
+        <NavLink href='/motos' onClick={onClose} inDrawer={true}>
+          Alquiler de Motos
+        </NavLink>
+        {isAuthenticated && (
+          <>
+            <NavLink href='/myMotos' onClick={onClose} inDrawer={true}>
+              Mis reservas y reseñas
+            </NavLink>
+            <NavLink href='/AddMoto' onClick={onClose} inDrawer={true}>
+              Añadir moto
+            </NavLink>
+          </>
+        )}
+        <NavLink href='/suscripcion' onClick={onClose} inDrawer={true}>
+          Ofertas Exclusivas
+        </NavLink>
+        <Box mt={4}>
+          {isAuthenticated ? (
             <Button
-              colorScheme='yellow'
-              color='var(--rtc-color-4)'
+              colorScheme='red'
               onClick={() => {
-                navigate('/login');
+                logout();
                 onClose();
               }}
               size='sm'
               width='full'
             >
-              Iniciar Sesión
+              Cerrar Sesión
             </Button>
-          )
-        )}
-      </Box>
-      <Box mt={4} ml='auto'>
+          ) : (
+            location.pathname !== '/login' && (
+              <Button
+                colorScheme='yellow'
+                color='var(--rtc-color-4)'
+                onClick={() => {
+                  navigate('/login');
+                  onClose();
+                }}
+                size='sm'
+                width='full'
+              >
+                Iniciar Sesión
+              </Button>
+            )
+          )}
+        </Box>
+      </Flex>
+
+      <Flex direction='row' justify='flex-end' width='100%' mt={4}>
         <ChangeTheme />
-      </Box>
+      </Flex>
     </Flex>
   </>
 );
